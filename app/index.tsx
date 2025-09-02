4e9cd107ef34350f07574549114e3d25dd77ca51
Matias Fleman <matufleman@gmail.com>
2025-09-02 14:37:26 -0300

    tarjeta2 agregada

--- app/index.tsx
+++ app/index.tsx
@@ -1,4 +1,5 @@
 import Tarjeta from "@/components/tarjeta";
+import Tarjeta2 from "@/components/tarjeta2";
 import { View } from "react-native";
 
 export default function Index() {
@@ -6,8 +7,8 @@ export default function Index() {
     <View>
       <Tarjeta texto={'Tarjeta 1'}/>
       <Tarjeta texto={'Tarjeta 2'}/>
-      <Tarjeta texto={'Tarjeta 3'}/>
-      <Tarjeta texto={'Tarjeta 4'}/>
+      <Tarjeta2 texto={'Tarjeta 3'}/>
+      <Tarjeta2 texto={'Tarjeta 4'}/>
     </View>
   );
 }
